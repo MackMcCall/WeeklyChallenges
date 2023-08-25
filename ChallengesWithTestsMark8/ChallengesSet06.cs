4a49ebfd9aecaff2865570c2cbb9d524c78f9417
Mack McCall <mackmc9924@gmail.com>
2023-08-25 09:06:49 -0600

    All Weekly Challenges Completed!

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,89 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Contains(null))
+                return false;
+            if (ignoreCase == false)
+            {
+                return words.Contains(word);
+            }
+            else
+            {
+                words = words.Select(x => x.ToLower());
+                word = word.ToLower();
+                return words.Contains(word);
+            }
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            if (str.Length == 1)
+                return 0;
+
+            if (str.All(x => x == str[0]))
+                return -1;
+
+            char unique = str.Last(x => str.Count(c => c == x) == 1);
+
+            return str.LastIndexOf(unique);
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+                return 0;
+            int counter = 1;
+            int maxConsec = 1;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] == numbers[i - 1])
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter = 1;
+                }
+                if (counter > maxConsec)
+                {
+                    maxConsec = counter;
+                }
+            }
+            return maxConsec;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || n < 1)
+            {
+                return new double[0];
+            }
+            
+            var nths = new List<double>();
+            
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if ((i + 1) % n == 0)
+                {
+                    nths.Add(elements[i]);
+                }
+            }
+            return nths.ToArray();
         }
     }
 }
